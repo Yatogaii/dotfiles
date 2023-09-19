4d3dda3ca885038acaed9ab686cf19c567d8e750
Yatogaii <781023956@qq.com>
2023-09-19 13:28:11 +0800

    add emacs EAF interleave mode

--- dotfiles/config/doom/config.el
+++ dotfiles/config/doom/config.el
@@ -350,7 +350,25 @@
 (add-to-list 'load-path "~/.config/emacs/.local/straight/repos/emacs-application-framework/")
 
   (use-package! eaf
-    :commands (eaf-open-browser eaf-open find-file)
+    :commands 
+      (eaf-open-browser
+       eaf-open
+       find-file
+       eaf-interleave-open-notes-file
+       eaf-interleave-add-note
+       eaf-interleave-mode
+       eaf-interleave-sync-current-note)
+    :custom
+    (eaf-find-alternate-file-in-dired t)
+    :diminish eaf-mode
+    :bind (:map eaf-interleave-mode-map
+           ("M-." . 'eaf-interleave-sync-current-note)
+           ("M-p" . 'eaf-interleave-sync-previous-note)
+           ("M-n" . 'eaf-interleave-sync-next-note)
+           :map eaf-interleave-app-mode-map
+           ("C-c M-i" . 'eaf-interleave-add-note)
+           ("C-c M-o" . 'eaf-interleave-open-notes-file)
+           ("C-c M-q" . 'eaf-interleave-quit))
     :config
       (require 'eaf-rss-reader)
       (require 'eaf-terminal)
@@ -361,5 +379,14 @@
       (require 'eaf-file-manager)
       (require 'eaf-video-player)
       (require 'eaf-git)
-      (require 'eaf-system-monitor))
+      (require 'eaf-system-monitor)
+      (require 'eaf-interleave)
+      ;; EAF interleave config
+      (add-hook 'eaf-pdf-viewer-hook 'eaf-interleave-app-mode)
+      (add-hook 'eaf-browser-hook 'eaf-interleave-app-mode)
+      (add-hook 'org-mode-hook 'eaf-interleave-mode)
+      (setq eaf-interleave-org-notes-dir-list '("~/org/interleave/"))
+      (setq eaf-interleave-split-direction 'vertical)
+      (setq eaf-interleave-disable-narrowing t)
+      (setq eaf-interleave-split-lines 20))
 
